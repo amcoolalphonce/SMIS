5b2d60ba39ad34766aa42067bbce76173c19ec8e
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-04 14:15:44 +0300

    Update smis.py

--- smis.py
+++ smis.py
@@ -4,12 +4,14 @@ from tkinter import  ttk
 win = tk.Tk()
 win.geometry("1350x700+0+0")
 win.title("University of Nairobi SMI System")
+win.config(bg="lightgreen")
 
 title_label = tk.Label(win , text="University of Nairobi SMI System", font=("Segoe UI", 30, "bold"),border=12., relief=tk.GROOVE, bg="blue", foreground="white")
 title_label.pack(side=tk.TOP, fill=tk.X)
-detail_frame = tk.LabelFrame(win, text="Enter Details", font=("Segoe UI", 20), bg="grey", foreground="black")
-detail_frame.place(x=20, y= 80, width=420, height=620)
+detail_frame = tk.LabelFrame(win, text="Enter Details", font=("Segoe UI", 20), bg="grey", foreground="black", bd=12, relief=tk.FLAT)
+detail_frame.place(x=20, y= 110, width=420, height=560)
 
+data_frame = tk.LabelFrame(win, bd=12, bg="lightgrey", relief=tk.RIDGE)
 
 
 win.mainloop()

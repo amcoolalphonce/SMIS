6a37f92ff98f5b9e9c0a519f25365cbc82a59cda
ALPHONCE OMONDI OMOLLO <82672024+amcoolalphonce@users.noreply.github.com>
2023-08-04 14:01:40 +0300

    Update smis.py

--- smis.py
+++ smis.py
@@ -5,8 +5,11 @@ win = tk.Tk()
 win.geometry("1350x700+0+0")
 win.title("University of Nairobi SMI System")
 
-title_label = tk.Label(win , text="University of Nairobi SMI System", font=("Segoe UI", 30, "bold"),border=12., relief=tk.GROOVE)
+title_label = tk.Label(win , text="University of Nairobi SMI System", font=("Segoe UI", 30, "bold"),border=12., relief=tk.GROOVE, bg="blue", foreground="white")
 title_label.pack(side=tk.TOP, fill=tk.X)
+detail_frame = tk.LabelFrame(win, text="Enter Details", font=("Segoe UI", 20), bg="grey", foreground="black")
+detail_frame.place(x=20, y= 80, width=420, height=620)
+
 
 
 win.mainloop()
